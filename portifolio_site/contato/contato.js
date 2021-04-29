dec026d2ed26ad3a93360ae3c865e7a6e644942a
André Jaques <67129593+Andre-Jaques@users.noreply.github.com>
2021-04-29 11:23:21 -0300

    Add files via upload

---
+++ portifolio_site/contato/contato.js
@@ -0,0 +1,16 @@
+
+    function executagit(){
+    window.location = "https://github.com/Andre-Jaques";
+    }
+
+    function executagmail(){
+        window.location = "https://gmail.com";
+        }
+
+    function executalinkedin(){
+        window.location = "https://www.linkedin.com/in/andr%C3%A9-ferreira-3186431b7/";
+        }
+
+    function executawhats(){
+        window.location = "https://web.whatsapp.com/";
+        }
